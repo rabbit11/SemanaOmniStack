26d36d4e55011e0e8baefad7077f6150d6cb1159
rabbit11 <pedro.coelho@dcomp.sor.ufscar.br>
2019-04-09 15:20:03 -0300

    back-end almost done

--- back-end/src/controllers/BoxController.js
+++ back-end/src/controllers/BoxController.js
@@ -2,7 +2,15 @@ const Box = require('../models/Box');
 
 class BoxController{
     async store(req, res){
-        const box = await Box.create({title: 'Rocketseat'});
+        const box = await Box.create(req.body);
+
+        return res.json(box);
+    }
+    async show(req, res){
+        const box = await Box.findById(req.params.id).populate({
+            path: 'files',
+            options: {sort: {createdAt: -1}}
+        });
 
         return res.json(box);
     }
